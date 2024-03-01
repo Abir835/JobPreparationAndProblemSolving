8134f381d018f1efd17f54ca0e92b87b6bb63cf6
abir835 <abirhasan.raj.bd@gmail.com>
2024-03-02 00:44:44 +0600

    add two number

---
+++ src/SimpleProlemsAndSolutions/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package SimpleProlemsAndSolutions;
+
+public class AddTwoNumbers {
+    private static int sumTwoNumber(int n, int m) {
+        return n+m;
+    }
+    public static void main(String[] args){
+        var n = 10;
+        var m = 20;
+        var result = sumTwoNumber(n,m);
+        System.out.println(result);
+    }
+}
